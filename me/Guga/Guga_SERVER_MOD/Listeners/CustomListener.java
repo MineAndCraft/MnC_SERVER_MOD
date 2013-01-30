76a50945bfbe35de721a2746a838f9adad57d095
Richard Kuba <rikub.cz@gmail.com>
2013-01-30 16:03:59 +0100

    Votifier bugfix

--- me/Guga/Guga_SERVER_MOD/Listeners/CustomListener.java
+++ me/Guga/Guga_SERVER_MOD/Listeners/CustomListener.java
@@ -31,7 +31,7 @@ public class CustomListener implements Listener
 		if((curr = plugin.FindPlayerCurrency(player)) != null)
 		{
 			curr.AddCurrency(5);
-			plugin.getServer().broadcastMessage(ChatColor.DARK_GREEN + "Hrac " + player + " ziskava 5 kreditu, 3 diamanty a 1 emerald za hlasovani. Hlasuj take.");
+			plugin.getServer().broadcastMessage(ChatColor.DARK_GREEN + "Hrac " + ChatColor.LIGHT_PURPLE + player + ChatColor.DARK_GREEN +" ziskava 5 kreditu, 3 diamanty a 1 emerald za hlasovani. Hlasuj take.");
 			if((p = plugin.getServer().getPlayer(player)) != null)
 			{
 				give(p);
@@ -41,11 +41,11 @@ public class CustomListener implements Listener
 	}
 	public void give(Player p)
 	{
-		p.getInventory().addItem(gift_DIAMOND);
-		p.getInventory().addItem(gift_EMERALD);
+		p.getInventory().addItem(new ItemStack(264, 3));
+		p.getInventory().addItem(new ItemStack(388, 1));
 	}
 	public ArrayList<String> notGifted= new ArrayList<String>();
 	public ItemStack gift_DIAMOND = new ItemStack(264, 3);
-	public ItemStack gift_EMERALD = new ItemStack(388, 1);
+	public ItemStack gift_EMERALD = new ItemStack(388, 3);
 	private Guga_SERVER_MOD plugin;
 }
