8d42df1d66436c717a1a2095cd982db5f3227116
Stanley Hawkeye <stanley.hawkeye@hotmail.com>
2013-03-19 21:43:41 +0100

    Removed CustomListener.notGifted since unused

--- me/Guga/Guga_SERVER_MOD/Listeners/CustomListener.java
+++ me/Guga/Guga_SERVER_MOD/Listeners/CustomListener.java
@@ -1,11 +1,6 @@
 package me.Guga.Guga_SERVER_MOD.Listeners;
 
-import java.util.ArrayList;
-
 import me.Guga.Guga_SERVER_MOD.Guga_SERVER_MOD;
-
-import com.vexsoftware.votifier.model.VotifierEvent;
-import com.vexsoftware.votifier.model.Vote;
 
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -14,12 +9,18 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 
+import com.vexsoftware.votifier.model.Vote;
+import com.vexsoftware.votifier.model.VotifierEvent;
+
 public class CustomListener implements Listener
 {
+	private Guga_SERVER_MOD plugin;
+	
 	public CustomListener(Guga_SERVER_MOD plugin)
 	{
 		this.plugin = plugin;
 	}
+	
 	@EventHandler(priority=EventPriority.NORMAL)
 	public void onVotifierEvent(VotifierEvent event) 
 	{
@@ -37,13 +38,13 @@ public class CustomListener implements Listener
 			
 		}
 	}
+	
 	public void give(Player p)
 	{
 		p.getInventory().addItem(new ItemStack(264, 1));
 		p.getInventory().addItem(new ItemStack(388, 1));
 	}
-	public ArrayList<String> notGifted= new ArrayList<String>();
-	public ItemStack gift_DIAMOND = new ItemStack(264, 1);
-	public ItemStack gift_EMERALD = new ItemStack(388, 1);
-	private Guga_SERVER_MOD plugin;
+
+	public final ItemStack gift_DIAMOND = new ItemStack(264, 1);
+	public final ItemStack gift_EMERALD = new ItemStack(388, 1);
 }
