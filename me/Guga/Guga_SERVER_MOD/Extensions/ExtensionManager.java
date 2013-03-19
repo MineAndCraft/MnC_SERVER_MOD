e4bb6c911819ad5f579fbcfa31ebd2954c439398
Stanley Hawkeye <stanley.hawkeye@hotmail.com>
2013-03-19 21:46:54 +0100

    SmsParser replaced by MessageListenerServer
    MessageListenerServer just receives and displays messages about
    purchases. PHP script now actually updates 'mnc_currency' and 'mnc_vip'
    tables in the database.

--- me/Guga/Guga_SERVER_MOD/Extensions/ExtensionManager.java
+++ me/Guga/Guga_SERVER_MOD/Extensions/ExtensionManager.java
@@ -1,14 +1,15 @@
 package me.Guga.Guga_SERVER_MOD.Extensions;
 
 import me.Guga.Guga_SERVER_MOD.Guga_SERVER_MOD;
+import me.Guga.Guga_SERVER_MOD.Extensions.MessageListenerServer;
 
 public class ExtensionManager
 {
 	private boolean _initialized = false;
 	private Guga_SERVER_MOD plugin;
 	
-	public SmsParser smsparser;
-	
+	public MessageListenerServer msgls;
+		
 	public ExtensionManager(Guga_SERVER_MOD plugin)
 	{
 		this.plugin = plugin;
@@ -18,15 +19,16 @@ public class ExtensionManager
 	public void initialize()
 	{
 		_initialized = true;
-		smsparser = new SmsParser();
-		smsparser.start();
+		//smsparser.start();
+		msgls = new MessageListenerServer();
+		msgls.start();		
 		
 		plugin.log.info("[GSM.ExtensionManager] initialized");
 	}
 	
 	public void disable()
 	{
-		smsparser.disable();
+		msgls.disable();
 		
 		_initialized = true;
 	}
