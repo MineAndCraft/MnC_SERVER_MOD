904c2cf11fcbf8efb10c8cfbc620680e9d5828f2
Stanley Hawkeye <stanley.hawkeye@hotmail.com>
2013-07-07 02:50:09 +0200

    Tell tab completion fixed (I suppose)

--- me/MnC/MnC_SERVER_MOD/chat/TellTabCompleter.java
+++ me/MnC/MnC_SERVER_MOD/chat/TellTabCompleter.java
@@ -1,11 +1,13 @@
 package me.MnC.MnC_SERVER_MOD.chat;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 import me.MnC.MnC_SERVER_MOD.MinecraftPlayer;
 import me.MnC.MnC_SERVER_MOD.UserManager;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
@@ -15,7 +17,7 @@ public class TellTabCompleter implements TabCompleter
 {
 	public List<String> onTabComplete(CommandSender sender, Command cmd, String line, String[] args)
 	{
-		List<String> tabCompletions = new LinkedList<String>();
+		LinkedList<String> tabCompletions = new LinkedList<String>();
 		if(sender instanceof Player)
 		{
 			MinecraftPlayer player = UserManager.getInstance().getUser(sender.getName());
@@ -30,6 +32,22 @@ public class TellTabCompleter implements TabCompleter
 					tabCompletions.add(tellsender);
 			}
 		}
+		if(args.length > 0)
+		{
+			for(Player p : Bukkit.getServer().getOnlinePlayers())
+			{
+				if(!tabCompletions.contains(p.getName()))
+					tabCompletions.add(p.getName());
+			}
+			Iterator<String> iterator =  tabCompletions.iterator();
+			while(iterator.hasNext())
+			{
+				String completion = iterator.next();
+				if(!completion.toLowerCase().startsWith(args[0].toLowerCase()))
+					iterator.remove();
+			}
+		}
+		
 		return tabCompletions;
 	}
 }
