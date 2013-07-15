f4419ae6861d4a18bf2e19f652b340ad6b03db49
Stanley Hawkeye <stanley.hawkeye@hotmail.com>
2013-07-15 19:41:33 +0200

    MobLimiter remake

--- me/MnC/MnC_SERVER_MOD/optimization/MobLimiter.java
+++ me/MnC/MnC_SERVER_MOD/optimization/MobLimiter.java
@@ -1,15 +1,12 @@
 package me.MnC.MnC_SERVER_MOD.optimization;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.logging.Logger;
 
 import me.MnC.MnC_SERVER_MOD.Config;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Monster;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
@@ -17,7 +14,6 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
-import org.bukkit.event.world.ChunkLoadEvent;
 
 public class MobLimiter implements Listener
 {
@@ -28,76 +24,34 @@ public class MobLimiter implements Listener
 		//do nothing
 	}
 	
-	@EventHandler(priority = EventPriority.MONITOR)
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
 	public void onEntitySpawn(CreatureSpawnEvent event)
 	{
+		Entity entity = event.getEntity();
+		Location location = event.getLocation();
+		
 		if(event.getSpawnReason() == SpawnReason.CUSTOM || event.getSpawnReason() == SpawnReason.EGG)
 			return;
 		
-		if(!event.getLocation().getWorld().getName().equalsIgnoreCase("world"))
-			return;
-		
-		fixTooManyMobs(event.getEntity());
-	}
-	
-	@EventHandler(priority = EventPriority.MONITOR)
-	public void onChunkLoad(ChunkLoadEvent event)
-	{
-		if(event.isNewChunk())
-			return;
-		
-		if(!event.getChunk().getWorld().getName().equalsIgnoreCase("world"))
-			return;
-		
-		for(Entity e : event.getChunk().getEntities())
-		{
-			fixTooManyMobs(e);
-		}
-		
-	}
-
-	/**
-	 * Kills concentrated mobs over limit
-	 * @param mob Entity around which to search
-	 */
-	private void fixTooManyMobs(Entity mob)
-	{
-		if(mob == null)
-			return;
-		
-		if(mob.getType() == EntityType.PLAYER || mob.getType() == EntityType.SHEEP)
-			return;
-		
-		if(!(mob instanceof Monster || mob instanceof Animals || mob instanceof Villager))
+		if(!location.getWorld().getName().equalsIgnoreCase("world"))
 			return;
 		
 		if(Config.MOB_LIMITER_LIMIT == 0)
 			return;
 		
-		//check for too many entities
-		List<Entity> entities = new LinkedList<Entity>();
-		for(Entity e : mob.getWorld().getEntities())
+		int count=0;		
+		for(Entity e : entity.getNearbyEntities(Config.MOB_LIMITER_LIMIT, Config.MOB_LIMITER_LIMIT, Config.MOB_LIMITER_LIMIT))
 		{
-			if(e.getType() == EntityType.PLAYER)
-				continue;
-			if(e.getType() != mob.getType())
-				continue;
-			if(e.getLocation().distance(mob.getLocation()) < Config.MOB_LIMITER_RADIUS)
-				entities.add(e);
+			if(e.getType() == entity.getType() && (e instanceof Monster || e instanceof Animals || e instanceof Villager))
+			{
+				count++;
+			}			
 		}
 		
-		if(entities.size() > Config.MOB_LIMITER_LIMIT)
+		if(count >= Config.MOB_LIMITER_LIMIT)
 		{
-			int toKill = entities.size()-Config.MOB_LIMITER_LIMIT;
-			log.info("Found "+entities.size()+" entities '"+mob.getType()+"' near '"+mob.getLocation().getWorld().getName()+"'["+mob.getLocation().getBlockX()+", "+mob.getLocation().getBlockY()+", "+mob.getLocation().getBlockZ()+"]. Triming by "+toKill+".");
-			for(int i=0;i < toKill;i++)
-			{
-				Entity e = entities.get(i);				
-				if(e instanceof LivingEntity)
-					((LivingEntity)e).damage(((LivingEntity) e).getHealth()+1);
-				e.remove();
-			}
-			log.info("Killed "+toKill+" entities '"+mob.getType()+"'.");
-		}
+			event.setCancelled(true);
+			log.info("Denied to spawn '"+entity.getType()+"' near "+location.getWorld().getName()+"["+location.getBlockX()+", "+location.getBlockY()+", "+location.getBlockZ()+"]");
+		}	
 	}
 }
